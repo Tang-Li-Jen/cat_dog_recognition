0633f0d51966058bf216065a18d8f03a9eb14f1a
Tang-Li-Jen <b00701160@gmail.com>
2019-04-21 23:45:31 +0800

    modify vgg16.py

--- vgg16.py
+++ vgg16.py
@@ -78,7 +78,7 @@ class Vgg16:
         self.flatten = tf.layers.flatten(self.pool5, name='flatten')
         self.fc6 = tf.layers.dense(self.flatten, 256, activation=tf.nn.relu, name='fc6')
         self.fc7 = tf.layers.dense(self.fc6, 128, activation=tf.nn.relu, name='fc7')
-        self.logits = tf.layers.dense(self.fc7, 1, activation=tf.nn.sigmoid, name='logit')
+        self.logits = tf.layers.dense(self.fc7, 1, activation=None, name='logit')
         # self.prediction = tf.layers.dense(self.fc7, 1, activation=tf.nn.sigmoid, name='prediction')
         self.loss = tf.nn.sigmoid_cross_entropy_with_logits(labels=_labels, logits=self.logits)
         self.optimizer = tf.train.RMSPropOptimizer(learning_rate=0.1).minimize(self.loss)
